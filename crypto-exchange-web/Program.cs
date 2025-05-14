f831fa6276dc9d356dab42f5d5c8948dbd3d95b4
Tejaswinee <tejaswinee@wildling.shoes>
2025-05-14 12:26:14 +0200

    issue accessing the json files, moved to the webservice project

--- crypto-exchange-web/Program.cs
+++ crypto-exchange-web/Program.cs
@@ -1,3 +1,6 @@
+using crypto_exchange.Services;
+using crypto_exchange_web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IExecutionPlanService, ExecutionPlanService>();
+builder.Services.AddSingleton<IOrderBookService, OrderBookService>();
+builder.Services.AddSingleton<IExchangeExecutionService, ExchangeExecutionService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
