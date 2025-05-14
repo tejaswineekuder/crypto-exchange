e37d6682f7d99783a7deb8ac728f5ebab1678dd4
Tejaswinee <tejaswinee@wildling.shoes>
2025-05-14 11:48:58 +0200

    Part 2: Adding Web service exposing the logic in console app. Endpoint execute with a post request having input parameters orderType and orderAmount will output best execution plans DTO added to access the input Accessing the logic via a service

---
+++ crypto-exchange-web/Controllers/ExecutiobPlanController.cs
@@ -0,0 +1,52 @@
+﻿using crypto_exchange_web.DTOs;
+using crypto_exchange_web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace crypto_exchange_web.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExecutiobPlanController : ControllerBase
+    {
+        private readonly ILogger<ExecutiobPlanController> _logger;
+        private readonly IExecutionPlanService _executionPlanService;
+
+        public ExecutiobPlanController(ILogger<ExecutiobPlanController> logger, IExecutionPlanService executionPlanService)
+        {
+            _logger = logger;
+            _executionPlanService = executionPlanService;
+        }
+
+        [HttpPost("execute")]
+        public async Task<IActionResult> ExecutePlan([FromForm] RequestDto request)
+        {
+            if (request.OrderAmount == null || request.OrderAmount == 0)
+            {
+                _logger.LogError("Order amount is invalid, cannot be processed further");
+                return BadRequest("Invalid Order amount.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.OrderType))
+            {
+                _logger.LogError("Order type is invalid, cannot be processed further");
+                return BadRequest("Invalid Order type.");
+            }
+            _logger.LogInformation("Input verified , moving further to fetch execution plan");
+
+            var executionPlan = await _executionPlanService.GetExecutionPlans(request);
+            if (executionPlan == null)
+            {
+                _logger.LogError("Error fetching execution plans!");
+                return BadRequest("Execution plans not found.");
+            }
+
+            _logger.LogInformation("Invoice evaluation summary : " + JsonConvert.SerializeObject(executionPlan));
+            return Ok(new
+            {
+                ExecutionPlan = executionPlan
+            });
+        }
+    }
+}
---
+++ crypto-exchange-web/DTOs/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crypto_exchange_web.DTOs
+{
+    public class RequestDto
+    {
+        [Required]
+        public decimal? OrderAmount { get; set; }
+
+        [Required]
+        public string? OrderType { get; set; }
+    }
+}
