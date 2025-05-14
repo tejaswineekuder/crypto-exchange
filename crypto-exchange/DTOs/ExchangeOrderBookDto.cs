bc58dd1e4b91b4444b37e7b4ed756766c9067b4b
Tejaswinee <tejaswinee@wildling.shoes>
2025-05-14 21:24:45 +0200

    added Json attributes to dtos added code to handle unprocessed order amount

--- crypto-exchange/DTOs/ExchangeOrderBookDto.cs
+++ crypto-exchange/DTOs/ExchangeOrderBookDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +9,43 @@ namespace crypto_exchange.DTOs
 {
     public class ExchangeOrderBookDto
     {
+        [JsonProperty("Id")]
         public string? Id { get; set; }
+
+        [JsonProperty("AvailableFunds")]
         public AvailableFunds? AvailableFunds { get; set; }
+
+        [JsonProperty("OrderBook")]
         public OrderBook? OrderBook { get; set; }
     }
 
     public class AvailableFunds
     {
+        [JsonProperty("Crypto")]
         public decimal? Crypto { get; set; }
+
+        [JsonProperty("Euro")]
         public decimal? Euro { get; set; }
     }
 
     public class Ask
     {
+        [JsonProperty("Order")]
         public OrderDto? Order { get; set; }
     }
 
     public class Bid
     {
+        [JsonProperty("Order")]
         public OrderDto? Order { get; set; }
     }
 
     public class OrderBook
     {
+        [JsonProperty("Bids")]
         public List<Bid>? Bids { get; set; }
+
+        [JsonProperty("Asks")]
         public List<Ask>? Asks { get; set; }
     }
 
--- crypto-exchange/DTOs/OrderDto.cs
+++ crypto-exchange/DTOs/OrderDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,22 @@ namespace crypto_exchange.DTOs
 {
     public class OrderDto
     {
+        [JsonProperty("Id")]
         public string? Id { get; set; }
+
+        [JsonProperty("Time")]
         public DateTime? Time { get; set; }
+
+        [JsonProperty("Type")]
         public string? Type { get; set; }
+
+        [JsonProperty("Kind")]
         public string? Kind { get; set; }
+
+        [JsonProperty("Amount")]
         public decimal? Amount { get; set; }
+
+        [JsonProperty("Price")]
         public decimal? Price { get; set; }
     }
 }
