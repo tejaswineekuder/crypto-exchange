c2de8bbe6734952c4532133005fb9d27d7c0f372
Tejaswinee <tejaswinee@wildling.shoes>
2025-05-14 10:42:10 +0200

    adding DTOs

---
+++ crypto-exchange/DTOs/ExchangeOrderBookDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crypto_exchange.DTOs
+{
+    public class ExchangeOrderBookDto
+    {
+        public string? Id { get; set; }
+        public AvailableFunds? AvailableFunds { get; set; }
+        public OrderBook? OrderBook { get; set; }
+    }
+
+    public class AvailableFunds
+    {
+        public decimal? Crypto { get; set; }
+        public decimal? Euro { get; set; }
+    }
+
+    public class Ask
+    {
+        public OrderDto? Order { get; set; }
+    }
+
+    public class Bid
+    {
+        public OrderDto? Order { get; set; }
+    }
+
+    public class OrderBook
+    {
+        public List<Bid>? Bids { get; set; }
+        public List<Ask>? Asks { get; set; }
+    }
+
+}
---
+++ crypto-exchange/DTOs/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crypto_exchange.DTOs
+{
+    public class OrderDto
+    {
+        public string? Id { get; set; }
+        public DateTime? Time { get; set; }
+        public string? Type { get; set; }
+        public string? Kind { get; set; }
+        public decimal? Amount { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
